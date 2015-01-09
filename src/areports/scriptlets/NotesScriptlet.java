12b0e755c0a50f92d90f142e706809b80180846a
Nathan <ns96@nyu.edu>
2015-01-09 12:42:17 -0500

    Finalized Scriptlets for Processing Notes

--- src/areports/scriptlets/NotesScriptlet.java
+++ src/areports/scriptlets/NotesScriptlet.java
@@ -7,22 +7,63 @@ package areports.scriptlets;
  */
 import net.sf.jasperreports.engine.JRDefaultScriptlet;
 import net.sf.jasperreports.engine.JRScriptletException;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class NotesScriptlet extends JRDefaultScriptlet {
+    /**
+     * Default constructor
+     */
     public NotesScriptlet() {}
     
+    /**
+     * This method extract out the relevant Information from the notes
+     * JSON record and sets the parameters in the script
+     * 
+     * @throws JRScriptletException 
+     */
     @Override
-    public void afterReportInit() throws JRScriptletException {
-        System.out.println("call afterReportInit()");
+    public void afterDetailEval() throws JRScriptletException {               
+        String jsonString = (String)getFieldValue("notesJSON");
+        String reportName = (String)getParameterValue("reportName");
         
-        String jsonString = "JSONText: " + (String)getFieldValue("note_notes");
+        JSONObject noteJS = new JSONObject(jsonString);
         
-        System.out.println("4 jsonText: " + jsonString);
-        setVariableValue("jsonText", "Variable modified by NotesScript");
-        // this.setVariableValue("someVar", new String("This variable value was modified by the scriptlet."));
+        if(reportName.equalsIgnoreCase("NamesRecordReport")) {
+            updateNamesRecordReport(noteJS);
+        } else {
+            System.out.println("Unsupported report: " + reportName);
+        }
     }
-
-    public String hello() throws JRScriptletException {
-        return "Hello! I'm the report's scriptlet object.";
+    
+    /**
+     * Method to set variable for the NamesReport
+     * 
+     * @param notesJS
+     * @throws JRScriptletException 
+     */
+    private void updateNamesRecordReport(JSONObject notesJS) throws JRScriptletException {
+         setVariableValue("descriptionType", notesJS.getString("label"));
+         
+         JSONArray subnotesJA = notesJS.getJSONArray("subnotes");
+         
+         for(int i = 0; i < subnotesJA.length(); i++) {
+             JSONObject subnoteJS = subnotesJA.getJSONObject(i);
+             
+             String type = subnoteJS.getString("jsonmodel_type");
+             
+             if(type.equals("note_text")) {
+                 setVariableValue("descriptionNote", subnoteJS.getString("content"));
+             } else {
+                 JSONArray contentJA = subnoteJS.getJSONArray("content");
+                 String content = "";
+                 
+                 for(int j = 0; j < contentJA.length(); j++) {
+                     content += " " + contentJA.getString(j);
+                 }
+                 
+                 setVariableValue("citation", content.trim());
+             }
+         }
     }
 }
